73ed1bb0e22626c7e592afff7985bd169eb8fabf
414645 <414645@bsd48.org>
2022-12-05 13:27:17 -0800

    plan

--- main.cpp
+++ main.cpp
@@ -10,6 +10,36 @@ using namespace std;
 //by Ryan Veith
 
 //this is the main class of the Zuul assignemnt
+/*
+4 by 4 = 16 rooms
+room.h
+room.cpp
+item.h
+item.cpp
+main.cpp
+
+room needs:
+"description"
+vector<item>
+(up to) 4 exits //everything will be 2d euclidian space
+
+item needs:
+"name"
+
+main needs:
+printItem(vector)
+printRoom(room)
+currentRoom somehow //2d so corinate grid if all else fails
+some wincondition
+
+commands
+GO (place) //north south east west
+HELP
+GET (item)
+DROP (item)
+QUIT
+  
+ */
 
 //void listInventory
 
