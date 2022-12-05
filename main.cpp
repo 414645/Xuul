fdd97108517c9acd9a18ea55b6c32b46797ec6e9
414645 <414645@bsd48.org>
2022-12-05 13:10:15 -0800

    first

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+//#ainclude <cstring
+
+//#include room.h
+//#include item.h
+
+using namespace std;
+
+//by Ryan Veith
+
+//this is the main class of the Zuul assignemnt
+
+//void listInventory
+
+int main() {
+  cout << "hello world" << endl;
+}
